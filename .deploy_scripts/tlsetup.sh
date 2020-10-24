934115ca4151319b9c3040342be6f102bee79d2f
danbjoseph <danbjoseph@gmail.com>
2020-10-23 20:12:44 -0400

    add missing deploy script

---
+++ .deploy_scripts/tlsetup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+
+# Update aptitude repository
+sudo apt-get --quiet update
+
+sudo apt-get install -y texlive-latex-extra \
+  texlive-fonts-recommended \
+  texlive-fonts-extra
+
+# This includes the recommended fonts, LaTeX packages, and the XeLaTeX
+# compiler
+# sudo apt-get install -y --no-install-recommends texlive-fonts-recommended \
+#     texlive-latex-extra \
+#     texlive-fonts-extra \
+#     dvipng \
+#     texlive-latex-recommended \
+#     texlive-xetex
+
+# Install decompression tool to extract LaTex packages later on
+# sudo apt-get install -y xzdec
+
+# Install biber for .bib compilation
+# sudo apt-get install -y biber
+
+# Need this for functioning of make latexpdf
+sudo apt-get install latexmk
+
+# Initialize user tree for installing additional packages
+# sudo tlmgr init-usertree
+
+# ----------------------------------------------------------------------------
+#   INSTALL ADDITIONAL PACKAGES HERE
+# ----------------------------------------------------------------------------
+
+# sudo tlmgr install \
+#     hyperref       \
+#     parskip        \
+#     babel          \
+#     tools          \
+#     graphics       \
+#     xcolor
